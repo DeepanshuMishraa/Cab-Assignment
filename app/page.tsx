99ef6faf2fb6638b1fb27b20619868ba23edebee
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-06-19 23:17:53 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/custom/Footer";
 import Hero from "@/components/custom/Hero";
 import Landing from "@/components/custom/Landing";
 import Navbar from "@/components/custom/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
